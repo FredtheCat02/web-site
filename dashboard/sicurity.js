fe3dd228187da300c62e0ca78aef715ed77be58f
FredtheCat02 <53126398+FredtheCat02@users.noreply.github.com>
2020-02-09 20:34:18 +0100

    Add files via upload

---
+++ dashboard/sicurity.js
@@ -0,0 +1,10 @@
+var getin = prompt("Inserire la password:","")
+    if (getin=="PassBang709")
+    {
+    alert('Benvenuto ;)')
+    }
+    else
+    {
+    alert("Password Sbagliata")
+    location.href='err.html'
+    }
